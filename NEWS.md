ebf3aded9b2278f4f4afba3b58733488b17e5e24
Michael Sumner <mdsumner@gmail.com>
2023-02-16 08:16:40 +0000

    reinstate mem()

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # dsn 0.0.1
 
+* New `mem()` function to reference an in memory array. 
+
 * New functions `sds()` and `vrtcon()`. 
 
 * First basic functions. 
--- R/mem.R
+++ R/mem.R
@@ -1,43 +1,51 @@
-#' ext_dim <- function (x, dim)
-#' {
-#'   px <- c(diff(x[1:2])/dim[1L], -diff(x[3:4])/dim[2L])
-#'   geo_transform0(px, c(x[1L], x[4L]))
-#' }
+ext_dim <- function (x, dim)
+{
+  px <- c(diff(x[1:2])/dim[1L], -diff(x[3:4])/dim[2L])
+  geo_transform0(px, c(x[1L], x[4L]))
+}
+
+geo_transform0 <- function (px, ul, sh = c(0, 0))
+{
+  c(xmin = ul[[1L]], xres = px[[1L]], yskew = sh[[2L]], ymax = ul[[2L]],
+    xskew = sh[[1L]], yres = px[[2L]])
+}
+
+#' Generate a data source name (DSN) for the GDAL MEM driver
 #'
-#' geo_transform0 <- function (px, ul, sh = c(0, 0))
-#' {
-#'   c(xmin = ul[[1L]], xres = px[[1L]], yskew = sh[[2L]], ymax = ul[[2L]],
-#'     xskew = sh[[1L]], yres = px[[2L]])
-#' }
+#' An array in memory can be referenced by a GDAL data source.
 #'
-#' #' Title
-#' #'
-#' #' @param x
-#' #' @param extent
-#' #'
-#' #' @return
-#' #' @export
-#' #'
-#' #' @examples
-#' #' m <- rep(1, 4)
-#' #' #vapour::vapour_raster_info(mem(m))
-#' #' vapour::vapour_read_raster(mem(m, dimension = c(2, 2)), native = TRUE)
-#' mem <- function(x, dimension = NULL, extent = NULL, bandoff = 0) {
-#'   x <- force(x)
 #'
-#'   if (is.null(extent)) extent <- c(0, dimension[1L], 0, dimension[2L])
-#'   d3 <- 1
-#'   if (length(dimension) > 2L) d3 <- dimension[3L]
-#'   type <- switch(typeof(x),
-#'                  integer = "Int32",
-#'                  double = "Float64",
-#'                  raw = "Byte")
-#'   gt <- ext_dim(extent, dimension)
-#'   addr <- as.double(pryr::address(x)) + bandoff
+#' This DSN will only work in R, and is only for use with GDAL read and query tools (so terra, sf, gdalcubes, vapour, etc.).
 #'
-#' out <- sprintf(
-#'   "MEM:::DATAPOINTER=%s,PIXELS=%i,LINES=%i,BANDS=%i,DATATYPE=%s,GEOTRANSFORM=%s",
-#'    addr, dimension[1L], dimension[2L], d3, type, paste(gt, collapse = "/"))
-#' print(out)
-#' out
-#' }
+#' @param x an R array, must be of numeric type (integer is converted to double)
+#' @param extent optional extent of the data in x,y c(xmin, xmax, ymin, ymax)
+#'
+#' @return character string, a DSN for use by GDAL
+#' @export
+#'
+#' @examples
+#' m <- matrix(c(0, 0, 0, 1), 5L, 4L)
+#' mem(m)
+#' mem(volcano)
+#'
+mem <- function(x, extent = NULL, bandoff = 0) {
+  x <- x * 1.0  ## make sure it's double haha
+#print(typeof(x))
+  dimension <- dim(x)
+  if (is.null(extent)) extent <- c(0, dimension[1L], 0, dimension[2L])
+  d3 <- 1
+  if (length(dimension) > 2L) d3 <- dimension[3L]
+  type <- "Float64"
+  # switch(typeof(x),
+  #                integer = "Int32",
+  #                double = "Float64",
+  #                raw = "Byte")
+  gt <- ext_dim(extent, dimension)
+  addr <- as.double(pryr::address(x)) + 48
+
+out <- sprintf(
+  "MEM:::DATAPOINTER=\"%s\",PIXELS=%i,LINES=%i,BANDS=%i,DATATYPE=%s,GEOTRANSFORM=%s,PIXELOFFSET=0,LINEOFFSET=0,BANDOFFSET=1",
+   addr, dimension[1L], dimension[2L], d3, type, paste(gt, collapse = "/"))
+#print(out)
+out
+}

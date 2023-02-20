ee842a3916f3b219203c4f5585440d26284230df
Michael Sumner <mdsumner@gmail.com>
2023-02-20 00:07:19 +0000

    MEM spatialref

--- R/mem.R
+++ R/mem.R
@@ -19,7 +19,7 @@ geo_transform0 <- function (px, ul, sh = c(0, 0))
 #'
 #' @param x an R array, must be of numeric type (integer is converted to double)
 #' @param extent optional extent of the data in x,y c(xmin, xmax, ymin, ymax)
-#'
+#' @param projection projection string (optional, sets the SPATIALREFERENCE of the MEM driver since GDAL 3.7)
 #' @return character string, a DSN for use by GDAL
 #' @export
 #'
@@ -28,7 +28,7 @@ geo_transform0 <- function (px, ul, sh = c(0, 0))
 #' mem(m)
 #' mem(volcano)
 #'
-mem <- function(x, extent = NULL, bandoff = 0) {
+mem <- function(x, extent = NULL, projection = "") {
   ## can't get Byte or Int32 to work
   type <- "Float64"
   x <- x * 1.0  ## make sure it's double haha
@@ -46,8 +46,15 @@ mem <- function(x, extent = NULL, bandoff = 0) {
   offset <- c(Int32 = 4, Float64 = 8, Byte = 1)[type]
   gt <- ext_dim(extent, dimension)
   addr <- as.double(pryr::address(x)) + 6 * offset
+  spatialref <- ""
 
-sprintf(
+  dsn <- sprintf(
   "MEM:::DATAPOINTER=\"%s\",PIXELS=%i,LINES=%i,BANDS=%i,DATATYPE=%s,GEOTRANSFORM=%s,PIXELOFFSET=0,LINEOFFSET=0,BANDOFFSET=1",
    addr, dimension[1L], dimension[2L], d3, type, paste(gt, collapse = "/"))
+  if (!is.null(projection) && length(projection) > 0 && !is.na(projection) && is.character(projection) && nzchar(projection)) {
+    spatialref <- sprintf("SPATIALREFERENCE=\"%s\"", projection[1L])
+    dsn <- sprintf("%s,%s", dsn, spatialref)
+  }
+  dsn
+
 }
